6bdeeb041cc354524241492ebaaed6c5bb7ac414
Elizabeth Nichols <elizabeth.nichols.95@gmail.com>
2015-03-16 19:38:25 -0400

    moved beginning and middle

--- js/main.js
+++ js/main.js
@@ -17,3 +17,25 @@ $storyExpand.on('click', function (){
 $story.toggleClass('js-panel-expand');
 
 });
+
+
+
+var $story2Expand = $('.story-2-expand');
+var $story2 = $('.story-2');
+
+$storyExpand.on('click', function (){
+
+$story2.toggleClass('js-panel-expand');
+
+});
+
+
+
+var $storyExpand = $('.story-expand');
+var $story = $('.story');
+
+$storyExpand.on('click', function (){
+
+$story.toggleClass('js-panel-expand');
+
+});
